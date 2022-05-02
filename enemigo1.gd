478ac79073224081619886cfd243e48d108804ee
Oriol Lopez <81315587+uriloop@users.noreply.github.com>
2022-05-02 19:58:56 +0200

    werfga

--- enemigo1.gd
+++ enemigo1.gd
@@ -1,15 +1,97 @@
 extends KinematicBody2D
 
+#const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICCION = 200
+export var WANDER_TARGET_RANGE = 4
 
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
 
-# Called when the node enters the scene tree for the first time.
+var DISTANCE = 120
+var velocidad = Vector2.ZERO
+var knockback = Vector2.ZERO
+
+var state = IDLE
+
+onready var sprite = $AnimatedSprite
+onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
+onready var hurtBox = $HurtBox
+onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
+onready var animationPlayer = $AnimationPlayer
+
 func _ready():
-	pass # Replace with function body.
+	state = pick_random_state([IDLE, WANDER])
 
+func _physics_process(delta):
+	knockback = knockback.move_toward(Vector2.ZERO, FRICCION * delta)
+	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocidad = velocidad.move_toward(Vector2.ZERO, FRICCION * delta)
+			seek_player()
+			if wanderController.get_time_left() == 0:
+				update_wander()
+		
+		WANDER:
+			seek_player()
+			if wanderController.get_time_left() == 0:
+				update_wander()
+			accelerate_towards_point(wanderController.target_position, delta)
+			
+			if global_position.distance_to(wanderController.target_position) <= WANDER_TARGET_RANGE:
+				update_wander()
+		
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				accelerate_towards_point(player.global_position, delta)
+			else:
+				state = IDLE
+	
+	if softCollision.is_colliding():
+		velocidad += softCollision.get_push_vector() * delta * 400
+	velocidad = move_and_slide(velocidad)
 
-func inicio(pos):
-	self.position = pos
+func update_wander():
+	state = pick_random_state([IDLE, WANDER])
+	wanderController.start_wander_timer(rand_range(1, 3))
+
+func accelerate_towards_point(point, delta):
+	var direccion = global_position.direction_to(point)
+	velocidad = velocidad.move_toward(direccion * MAX_SPEED, ACCELERATION * delta)
+	sprite.flip_h = velocidad.x < 0
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
+
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
+
+func _on_HurtBox_area_entered(area):
+	stats.health -= area.damage
+	knockback = area.knockback_vector * DISTANCE
+	hurtBox.create_hit_effect()
+	hurtBox.start_invencibility(0.4)
+
+func _on_Stats_no_health():
+	queue_free()
+	#var enemyDeathEffect = EnemyDeathEffect.instance()
+	#get_parent().add_child(enemyDeathEffect)
+	#enemyDeathEffect.global_position = global_position
+
+func _on_HurtBox_invincibility_started():
+	animationPlayer.play("Start")
+
+func _on_HurtBox_invincibility_ended():
+	animationPlayer.play("Stop")
