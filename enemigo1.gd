2b7aa91ea6ccb4af8d19ce91267578f3dda4ced1
Oriol Lopez <81315587+uriloop@users.noreply.github.com>
2022-05-02 20:31:05 +0200

    asergvawer

--- enemigo1.gd
+++ enemigo1.gd
@@ -7,28 +7,20 @@ export var MAX_SPEED = 50
 export var FRICCION = 200
 export var WANDER_TARGET_RANGE = 4
 
-enum {
-	IDLE,
-	WANDER,
-	CHASE
-}
+
 
 var DISTANCE = 120
 var velocidad = Vector2.ZERO
 var knockback = Vector2.ZERO
 
-var state = IDLE
 
-onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $player_detection_zone
-onready var hurtBox = $HurtBox
-onready var softCollision = $SoftCollision
-onready var wanderController = $WanderController
+
 onready var animationPlayer = $AnimationPlayer
 
 func _ready():
-	state = pick_random_state([IDLE, WANDER])
+	pass
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICCION * delta)
@@ -37,35 +29,17 @@ func _physics_process(delta):
 	var player = playerDetectionZone.player
 	if player != null:
 		accelerate_towards_point(player.global_position, delta)
-	else:
-		state = IDLE
-	
-	if softCollision.is_colliding():
-		velocidad += softCollision.get_push_vector() * delta * 400
-	velocidad = move_and_slide(velocidad)
 
-func update_wander():
-	state = pick_random_state([IDLE, WANDER])
-	wanderController.start_wander_timer(rand_range(1, 3))
+	
+	
+
 
 func accelerate_towards_point(point, delta):
 	var direccion = global_position.direction_to(point)
 	velocidad = velocidad.move_toward(direccion * MAX_SPEED, ACCELERATION * delta)
-	sprite.flip_h = velocidad.x < 0
 
-func seek_player():
-	if playerDetectionZone.ver_jugador():
-		state = CHASE
 
-func pick_random_state(state_list):
-	state_list.shuffle()
-	return state_list.pop_front()
 
-func _on_HurtBox_area_entered(area):
-	stats.health -= area.damage
-	knockback = area.knockback_vector * DISTANCE
-	hurtBox.create_hit_effect()
-	hurtBox.start_invencibility(0.4)
 
 func _on_Stats_no_health():
 	queue_free()
@@ -73,11 +47,6 @@ func _on_Stats_no_health():
 	#get_parent().add_child(enemyDeathEffect)
 	#enemyDeathEffect.global_position = global_position
 
-func _on_HurtBox_invincibility_started():
-	animationPlayer.play("Start")
-
-func _on_HurtBox_invincibility_ended():
-	animationPlayer.play("Stop")
 
 
 
