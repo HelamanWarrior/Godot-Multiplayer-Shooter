8244422d630b61ba15ba94ab5c473a3235c1eea6
Oriol Lopez <81315587+uriloop@users.noreply.github.com>
2022-05-02 20:21:55 +0200

    RFTGHS

--- enemigo1.gd
+++ enemigo1.gd
@@ -34,28 +34,11 @@ func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICCION * delta)
 	knockback = move_and_slide(knockback)
 	
-	match state:
-		IDLE:
-			velocidad = velocidad.move_toward(Vector2.ZERO, FRICCION * delta)
-			seek_player()
-			if wanderController.get_time_left() == 0:
-				update_wander()
-		
-		WANDER:
-			seek_player()
-			if wanderController.get_time_left() == 0:
-				update_wander()
-			accelerate_towards_point(wanderController.target_position, delta)
-			
-			if global_position.distance_to(wanderController.target_position) <= WANDER_TARGET_RANGE:
-				update_wander()
-		
-		CHASE:
-			var player = playerDetectionZone.player
-			if player != null:
-				accelerate_towards_point(player.global_position, delta)
-			else:
-				state = IDLE
+	var player = playerDetectionZone.player
+	if player != null:
+		accelerate_towards_point(player.global_position, delta)
+	else:
+		state = IDLE
 	
 	if softCollision.is_colliding():
 		velocidad += softCollision.get_push_vector() * delta * 400
