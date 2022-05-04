dcd326aca015a8995a25568e410e3fa0bb948e32
Oriol Lopez <81315587+uriloop@users.noreply.github.com>
2022-05-04 16:54:09 +0200

    eagrf

--- Global.gd
+++ Global.gd
@@ -7,11 +7,20 @@ var alive_players = []
 
 
 
+func instance_enemy_at_location(node: Object, location: Vector2) -> Object:
+	var node_instance = instance_enemy(node)
+	node_instance.global_position = location
+	return node_instance
+
 func instance_node_at_location(node: Object, parent: Object, location: Vector2) -> Object:
 	var node_instance = instance_node(node, parent)
 	node_instance.global_position = location
 	return node_instance
 
+func instance_enemy(node: Object) -> Object:
+	var node_instance = node.instance()
+	$Game.add_child(node_instance)
+	return node_instance
 func instance_node(node: Object, parent: Object) -> Object:
 	var node_instance = node.instance()
 	parent.add_child(node_instance)
