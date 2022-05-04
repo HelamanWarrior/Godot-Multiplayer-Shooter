fa898ecc5b1a0a4f45e278d09e35f1607d931d62
Oriol Lopez <81315587+uriloop@users.noreply.github.com>
2022-05-04 16:44:15 +0200

    Update Global.gd

--- Global.gd
+++ Global.gd
@@ -5,10 +5,6 @@ var ui = null
 
 var alive_players = []
 
-func instance_node_at_location(node: Object, parent: Object, location: Vector2) -> Object:
-	var node_instance = instance_node(node, parent)
-	node_instance.global_position = location
-	return node_instance
 
 
 func instance_node_at_location(node: Object, parent: Object, location: Vector2) -> Object:
