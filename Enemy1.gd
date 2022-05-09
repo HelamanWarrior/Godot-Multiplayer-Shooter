e3a54af6a57db90dd3c7a0015d8f2e48105017f2
Oriol Lopez <81315587+uriloop@users.noreply.github.com>
2022-05-09 23:48:59 +0200

    awd

--- Enemy1.gd
+++ Enemy1.gd
@@ -9,23 +9,34 @@ const speed = 200
 var velocity = Vector2()
 var playerSeeking = null
 
+puppet var puppet_position setget puppet_position_set
 puppet var puppet_velocity = Vector2()
 puppet var puppet_rotation = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	yield(get_tree(), "idle_frame")
+	
+	if get_tree().has_network_peer():
+		if is_network_master():
+			velocity = Vector2(0,0)
+			rotation = 0
+			rset("puppet_velocity", velocity)
+			rset("puppet_rotation", rotation)
+			rset("puppet_position", global_position)
+
+func puppet_position_set(new_value) -> void:
+	puppet_position = new_value
+	global_position = puppet_position
 
 
 func _process(delta):
-	if get_tree().is_network_server():
-		if (playerSeeking != null):
-			position= (playerSeeking.position - position).normalized()
-			velocity = move_and_slide(position * speed)
-			look_at(playerSeeking.position)
-			#Falta updatear en los otros clientes
-
-
+	if get_tree().has_network_peer():
+		if is_network_master():
+			global_position += velocity * speed * delta
+		else:
+			rotation = puppet_rotation
+			global_position += puppet_velocity * speed * delta
 
 sync func newPlayerSeeking(playerToSeek):
 	for child in Persistent_nodes.get_children():
